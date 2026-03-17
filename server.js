bca77964558779c5a0177675f9f3a7c001418925
Patrick Souza <patrick.souza@superitalo.com.br>
2026-03-17 09:00:50 -0300

    vai

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.static(__dirname));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Rota raiz - servir index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Criar pasta de fotos se não existir
 const photosDir = path.join(__dirname, 'photos');
 if (!fs.existsSync(photosDir)) {
